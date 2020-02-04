bdc2f3e895488879893ae8e188ba55bd0075acc8
yuga sai srinivasu gottumukkala <36449483+yugasaisrinivasGottumukkala@users.noreply.github.com>
2020-02-05 01:50:47 +0530

    Update views.py

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,14 +1,38 @@
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import City
 from .forms import CityForm
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=457298abe44c2e74c32043718010fd10'
 
+    err_msg = ''
+    message = ''
+    message_class = ''
+
     if request.method == 'POST':
         form = CityForm(request.POST)
-        form.save()
+
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            existing_city_count = City.objects.filter(name=new_city).count()
+            
+            if existing_city_count == 0:
+                r = requests.get(url.format(new_city)).json()
+
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    err_msg = 'City does not exist in the world!'
+            else:
+                err_msg = 'City already exists in the database!'
+
+        if err_msg:
+            message = err_msg
+            message_class = 'is-danger'
+        else:
+            message = 'City added successfully!'
+            message_class = 'is-success'
 
     form = CityForm()
 
@@ -29,5 +53,16 @@ def index(request):
 
         weather_data.append(city_weather)
 
-    context = {'weather_data' : weather_data, 'form' : form}
-    return render(request,'weather/weather.html', context)
+    context = {
+        'weather_data' : weather_data, 
+        'form' : form,
+        'message' : message,
+        'message_class' : message_class
+    }
+
+    return render(request, 'weather/weather.html', context)
+
+def delete_city(request, city_name):
+    City.objects.get(name=city_name).delete()
+    
+    return redirect('home')
